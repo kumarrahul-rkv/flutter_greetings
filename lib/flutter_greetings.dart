e8dfa6e2fa8d639830d903c3752b2808d3c3d081
Aydcoder <aydcoder@gmail.com>
2022-12-06 10:38:04 +0100

    Initial commit

--- lib/flutter_greetings.dart
+++ lib/flutter_greetings.dart
@@ -7,7 +7,7 @@ class YonoGreetings {
       if (timeNow <= 12) {
         return 'Good Morning';
       } else if ((timeNow > 12) && (timeNow <= 16)) {
-        return 'Good Afternoon';
+        return 'Good Afternoon  ';
       } else if ((timeNow > 16) && (timeNow <= 20)) {
         return 'Good Evening';
       } else {
